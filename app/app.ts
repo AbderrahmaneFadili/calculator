f471fbfe2b6dd1b0896575a296e8214242a92df4
Abderrahmane Fadili <abdouelrfadili@gmail.com>
2022-07-12 11:05:57 +0100

    Using Deffrent Colors for styling

--- app/app.ts
+++ app/app.ts
@@ -1,6 +1,9 @@
 import PromptSync, { Prompt } from 'prompt-sync';
 import * as math from 'mathjs';
 import formatOperation from './helper/formatOperation';
+import colors from 'colors';
+
+colors.enable();
 
 //promt configuration
 let prompt: Prompt = PromptSync({
@@ -8,8 +11,8 @@ let prompt: Prompt = PromptSync({
 });
 
 console.log('\n');
-console.log(" ######### Calculator  ##########\n");
-let operation: string = prompt(" Enter a calculation operation ex : ((12 / 4 + 3) x 7) : ");
+console.log("          ######### Calculator  ##########\n".cyan);
+let operation: string = prompt("          Enter a calculation operation ex : ((12 / 4 + 3) x 7) : ".gray);
 let result: any = null;
 console.log("\n");
 
@@ -17,13 +20,15 @@ try {
 
   result = math.evaluate(formatOperation(operation));
   if (result === Infinity) {
-    console.log(" Wrong Operation");
+    console.log("          Wrong Operation".yellow);
   } else {
-    console.log(` The result is ${result}`);
+    console.log(`          The result is ${result}`.green);
   }
 
 } catch (error) {
-  console.error(" Invalid Operation");
+  console.error("          Invalid Operation".red);
 }
 
 console.log("\n");
+
+
